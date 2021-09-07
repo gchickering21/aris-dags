aa3231c04f8f36e51b190cb02708a002881d8426
Eric Buehler <buehlere77@gmail.com>
2021-09-07 15:39:30 -0400

    fixing name

--- eb_ssh_test.py
+++ eb_ssh_test.py
@@ -18,7 +18,7 @@ default_args = {
 }
 
 
-dag = DAG(dag_id='amt1_ssh_operator',
+dag = DAG(dag_id='eb_ssh_test',
           default_args=default_args,
         #   schedule_interval='0,10,20,30,40,50 * * * *',
           dagrun_timeout=timedelta(seconds=120))
