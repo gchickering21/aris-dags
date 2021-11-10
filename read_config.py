df836cd5a59ba63c3cd6ecd37997b88abca49667
Eric Buehler <buehlere77@gmail.com>
2021-11-10 10:51:40 -0500

    trying to get config working

--- read_config.py
+++ read_config.py
@@ -3,7 +3,7 @@ import configparser
 
 # set-up parsers and read 
 config = configparser.ConfigParser()
-config.read("./config.ini")
+config.read("config.ini")
 
 # read git path 
 SERVICE_GIT_DIR = config.get('DIRECTORIES', 'SERVICE_GIT_DIR')
