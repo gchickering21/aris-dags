3f6305a197fd4fe42363b987a7d1f972e9393304
Eric Buehler <buehlere77@gmail.com>
2022-03-02 12:42:53 -0500

    creating state DAG

---
+++ aris_ccd_state.py
@@ -0,0 +1,143 @@
+from datetime import timedelta, datetime
+from pickle import TRUE
+import airflow
+import code_executer
+from airflow import DAG
+from airflow.operators.python import PythonOperator, PythonVirtualenvOperator
+from airflow.operators.python_operator import BranchPythonOperator
+from airflow.contrib.operators.ssh_operator import SSHOperator
+from airflow.contrib.hooks.ssh_hook import SSHHook
+SERVICE_GIT_DIR = 'C:\\ARIS\\autoDigest\\ccd' # File housing ARIS repos on SAS server's C drive
+
+# default args
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['ebuehler@air.org', 'mtrihn@air.org'],
+    'email_on_failure': TRUE,
+    'email_on_retry': False,
+    'start_date': datetime.now() - timedelta(minutes=20),
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Define Main DAG for CCD pipeline 
+dag = DAG(dag_id='aris_ccd_state',
+          default_args=default_args,
+        #   schedule_interval='0,10,20,30,40,50 * * * *',
+          dagrun_timeout=timedelta(seconds=600))
+
+def links():
+    '''
+    Purpose: execute ccd_data_list_downloader.py  on command line to generate list of CCD links
+    '''
+    ssh = SSHHook(ssh_conn_id="sas1buehlere")
+    ssh_client = None
+    print(ssh)
+    try:
+        ssh_client = ssh.get_conn()
+        ssh_client.load_system_host_keys()
+        command = 'cd ' +  SERVICE_GIT_DIR + ' && python ' + '\\IO\\ccd_data_list_downloader.py' 
+        stdin, stdout, stderr = ssh_client.exec_command(command)
+        out = stdout.read().decode().strip()
+        error = stderr.read().decode().strip()
+        print(out)
+        print(error)
+    finally:
+        if ssh_client:
+            ssh_client.close()
+
+
+def dat():
+    '''
+    Purpose: execute ccd_data_downloader.py on command line to download CCD data 
+    '''
+    ssh = SSHHook(ssh_conn_id="sas1buehlere")
+    ssh_client = None
+    print(ssh)
+    try:
+        
+        ssh_client = ssh.get_conn()
+        ssh_client.load_system_host_keys()
+        command = 'cd ' +  SERVICE_GIT_DIR + ' && python ' +  'IO\\ccd_data_downloader.py'
+        stdin, stdout, stderr = ssh_client.exec_command(command)
+        out = stdout.read().decode().strip()
+        error = stderr.read().decode().strip()
+        print(out)
+        print(error)
+    finally:
+        if ssh_client:
+            ssh_client.close()
+
+
+
+def nonfiscal():
+    '''
+    Purpose: execute ccd_nonfiscal_state_RE2.sas on command line to generate nonfiscal long data from ccd data 
+    '''
+    ssh = SSHHook(ssh_conn_id="sas1buehlere")
+    ssh_client = None
+    print(ssh)
+    try:
+        ssh_client = ssh.get_conn()
+        ssh_client.load_system_host_keys()
+        command = 'cd ' +  SERVICE_GIT_DIR + '\\SAS' + ' && sas ccd_nonfiscal_state_RE2'
+        stdin, stdout, stderr = ssh_client.exec_command(command)
+        out = stdout.read().decode().strip()
+        error = stderr.read().decode().strip()
+        print(out)
+        print(error)
+    finally:
+        if ssh_client:
+            ssh_client.close()
+
+def nonfiscal_wide():
+    '''
+    Purpose: execute ccd_school_convert.sas on command line to generate nonfiscal wide data from nonfiscal long data. 
+    '''
+    ssh = SSHHook(ssh_conn_id="sas1buehlere")
+    ssh_client = None
+    print(ssh)
+    try:
+        ssh_client = ssh.get_conn()
+        ssh_client.load_system_host_keys()
+        command = 'cd ' +  SERVICE_GIT_DIR + '\\SAS' + ' && sas ccd_school_convert'
+        stdin, stdout, stderr = ssh_client.exec_command(command)
+        out = stdout.read().decode().strip()
+        error = stderr.read().decode().strip()
+        print(out)
+        print(error)
+    finally:
+        if ssh_client:
+            ssh_client.close()
+
+# Download CCD Links 
+download_links = PythonOperator(
+    task_id='download_links',
+    python_callable=links,
+    dag=dag
+)
+
+# Download CCD Data 
+download_dat = PythonOperator(
+    task_id='download_dat',
+    python_callable=dat,
+    dag=dag
+)
+
+# Generate Nonfiscal state from CCD Data with SAS
+gen_nonfiscal = PythonOperator(
+    task_id='gen_nonfiscal',
+    python_callable=nonfiscal,
+    dag=dag
+)
+
+# Generate Nonfiscal wide file with SAS 
+gen_nonfiscal_wide = PythonOperator(
+    task_id='gen_nonfiscal_wide',
+    python_callable=nonfiscal_wide,
+    dag=dag
+)
+
+# DAG Dependancy
+download_links >> download_dat >> gen_nonfiscal >> gen_nonfiscal_wide 
